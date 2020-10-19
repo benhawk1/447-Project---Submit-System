b20b191a9e180c9d36c8b892a160c2dd014e2b95
NickP421 <70792803+NickP421@users.noreply.github.com>
2020-10-19 15:09:11 -0400

    Cleaned up functions, made submit_file independent

--- submission_backend.py
+++ submission_backend.py
@@ -9,21 +9,44 @@ from datetime import datetime, timezone
 ###Constants
 #####################
 TIMEZONE = "US/Eastern"
+MONGODB_URL = "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
+PORT_NUM = 27017
 
 #####################
 
-def get_timestamp():
-    #first get UTC time, then convert to EST to avoid issues with daylight savings
-    utc = pytz.timezone("UTC")
-    now = utc.localize(datetime.utcnow())
-    
-    tz = pytz.timezone(TIMEZONE)
-    local_time = now.astimezone(tz)
-    
-    dt_string = local_time.strftime("%m/%d/%Y %H:%M:%S")
-    return dt_string
+def connect_client():
+    try:
+        client = pymongo.MongoClient('localhost')#MONGODB_URL, PORT_NUM)
+        print("Client Connection successful")
+        return client
+    except:
+        print("Client Connection failed")
+        return -1;
 
-def submit_file(collection, filename):
+def connect_db(client):
+    db = client.student_submissions
+    return db
+
+def connect_collection(db, coll_name):
+    try:
+        collection = db[coll_name]
+        print("Collection accessed")
+    except:
+        collection = client.db.create_collection(coll_name)
+        print("Collection doesn't exist; new one created")
+
+    return collection
+
+def submit_file(filename, coll_name):
+    #connect to database, access collection
+    client = connect_client()
+    if client == -1:
+        return -1
+    
+    db = connect_db(client)
+
+    collection = connect_collection(db, coll_name)
+        
     submission_dict = {}
     with open(filename, 'rb') as f:
         submission_dict["filename"] = filename
@@ -40,6 +63,39 @@ def submit_file(collection, filename):
     
     return collection.insert_one(submission_dict)
 
+def list_collection(coll_name):
+    client = connect_client()
+    if client == -1:
+        return -1
     
+    db = connect_db(client)
+
+    collection = connect_collection(db, coll_name)
+
+    #print all documents in collection
+    cursor = collection.find({})
+    for document in cursor:
+        print(document)
+
+def drop_collection(coll_name):
+    client = connect_client()
+    if client == -1:
+        return -1
+    
+    db = connect_db(client)
+
+    collection = connect_collection(db, coll_name)
+
+    collection.drop()
 
 
+def get_timestamp():
+    #first get UTC time, then convert to EST to avoid issues with daylight savings
+    utc = pytz.timezone("UTC")
+    now = utc.localize(datetime.utcnow())
+    
+    tz = pytz.timezone(TIMEZONE)
+    local_time = now.astimezone(tz)
+    
+    dt_string = local_time.strftime("%m/%d/%Y %H:%M:%S")
+    return dt_string
--- submission_backend_tests.py
+++ submission_backend_tests.py
@@ -12,9 +12,8 @@ from submission_backend import * #get functions from submission_backend.py
 ###################
 ###Constants
 ###################
-MONGODB_URL = "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
-PORT_NUM = 27017
-FILENAME_TEST = "hello_world.py"
+
+FILENAME_TEST = "help.txt"
 COLLECTION_NAME = "student_submissions"
 
 
@@ -23,26 +22,17 @@ COLLECTION_NAME = "student_submissions"
 
 
 if __name__ == "__main__":
-    try:
-        client = pymongo.MongoClient(MONGODB_URL, PORT_NUM)
-        print("Connection successful")
-    except:
-        print("Connection failed")
-        
-    #access student_submissions database 
-    db = client.student_submissions
 
-    try:
-        collection = client.db.create_collection(COLLECTION_NAME)
-        print("Student Submission collection created")
-    except:
-        collection = db[COLLECTION_NAME]
-        print("Student Submission collection already exists")
-
-    return_obj = submit_file(collection, FILENAME_TEST)
+    
+    return_obj = submit_file(FILENAME_TEST, COLLECTION_NAME)
     print("This should print True: ", return_obj.acknowledged)
     print("bson ObjectID: ", return_obj.inserted_id)
+
+    list_collection(COLLECTION_NAME)    
+
+    #clean up collection for testing if needed
+    #drop_collection(COLLECTION_NAME)
+
     
-    cursor = collection.find({})
-    for document in cursor:
-        print(document)
+
+    
