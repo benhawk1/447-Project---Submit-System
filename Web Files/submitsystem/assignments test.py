8458e792b590e963b2f86463a46c7c3113b40638
benhawk1 <60792806+benhawk1@users.noreply.github.com>
2020-11-16 17:04:41 -0500

    Add files via upload

--- Web Files/submitsystem/assignments test.py
+++ Web Files/submitsystem/assignments test.py
@@ -1,5 +1,21 @@
-import assignments
+from assignments import *
 import pandas as pd
+
+#test datetime function
+
+MONTH = "12"
+DAY = "31"
+YEAR = "2021"
+HOUR = "23"
+MINUTE = "59"
+
+
+#should print out Date in YYYY-MM-DD format, then
+#Time in HH:MM:SS format, then both combined
+duedate_to_datetime(MONTH, DAY, YEAR, HOUR, MINUTE)
+
+
+
 loginInfo = pd.read_csv("C:/Users/benha/Documents/CMSC 447 Project/Users.csv")
 
 # This program will test the usage of the functions within assignments.py. Once this function runs, it will ask for the
@@ -7,6 +23,7 @@ loginInfo = pd.read_csv("C:/Users/benha/Documents/CMSC 447 Project/Users.csv")
 # ask for a section number or 'All'. If the incorrect value is given, it will reprompt. Otherwise, it will ask for a
 # name for the new assignment, and then a date and time for the assignment. If the values given for the date and time
 # are invalid, then the user will be reprompted accordingly.
-assignments.addAssignment(0, loginInfo)
+addAssignment(0, loginInfo)
+
 
 
