ec2914e94a8373bc939221a5121044b0bca15764
Nicholas Proulx <nproulx1@umbc.edu>
2020-11-28 15:19:27 -0500

    Gets latest submission, warns user if submission is late, updated function calls

--- Web Files/submitsystem/submission_backend.py
+++ Web Files/submitsystem/submission_backend.py
@@ -2,7 +2,7 @@ import pytz
 import os
 from bson.binary import Binary
 from datetime import datetime
-from db_func import *
+from submitsystem.db_func import *
 
 #####################
 ###Constants
@@ -13,7 +13,7 @@ PORT_NUM = 27017
 
 #####################
 
-def submit_file(student_ID, section, filepath, coll_name):
+def submit_file(student_ID, assignment_ID, section, filepath, coll_name):
     #connect to database, access collection
     client = connect_client()
     if client == -1:
@@ -26,6 +26,18 @@ def submit_file(student_ID, section, filepath, coll_name):
     #iterate over section documents
     for doc in collection.find({}):
         if doc["section"] == section:
+            #make sure assignment_ID matches somewhere in assignments list
+            does_assignment_exist = False
+            for i in range(len(doc["assignments"])):
+                if doc["assignments"][i]["ID"]:
+                    does_assignment_exist = True
+                    #get due date from assignment to compare to submission timestamp
+                    due_date = doc["assignments"][i]["due date"]
+
+            if not does_assignment_exist:
+                print("Assignment does not exist")
+                return -1
+                    
             #iterate over list of student dictionaries
             for i in range(len(doc["students"])):
                 #if the ith student dict has a matching ID, we've found our student
@@ -37,10 +49,11 @@ def submit_file(student_ID, section, filepath, coll_name):
                     f.close()
 
                     filename = os.path.basename(filepath)
-                    time_string = get_timestamp()
+                    time = get_timestamp()
                     submission_dict = {"filename" : filename,
+                                       "assignment_ID": assignment_ID,
                                        "file" : encoded ,
-                                       "timestamp" : time_string}
+                                       "timestamp" : time}
 
                     new_doc = doc
                     new_doc["students"][i]["submissions"].append(submission_dict)
@@ -49,14 +62,57 @@ def submit_file(student_ID, section, filepath, coll_name):
                     #second parameter is new document
                     #upsert parameter will insert if doc is not found in def
                     collection.update_one({"section": section}, {"$set" : new_doc}, upsert=False)
+
+                    #tell user if their submission was late or not
+                
+                    time = time.replace(tzinfo=None) #needed to compare timestamp to duedate
+
+                    if due_date < time:
+                        print("Warning: Your submission was late!")
+                    
                     return 0
                 
             print("student ID not found")
             return -1
     print("section not found")
     return -1
+   
+            
+def get_latest_submission(student_ID, section, coll_name):
+    #connect to database, access collection
+    client = connect_client()
+    if client == -1:
+        return -1
+    
+    db = connect_db(client)
+
+    collection = connect_collection(db, coll_name)
+
+    for doc in collection.find({}):
+        if doc["section"] == section:
+            #iterate over list of student dictionaries
+            for i in range(len(doc["students"])):
+                #if the ith student dict has a matching ID, we've found our student
+                if doc["students"][i]["ID"] == student_ID:
+                    submissions = doc["students"][i]["submissions"]
+
+                    #if there are no submissions, return empty dict
+                    if len(submissions) == 0:
+                        return {}
+                    #find most recent timestamp in submissions dict
+                    curr_sub = submissions[0]
+                    for submission in submissions:
+                        if submission["timestamp"] < curr_sub["timestamp"]:
+                            curr_sub = submission
+                    return curr_sub
+
+                print("student ID not found")
+                return -1
+    print("section not found")
+    return -1
                     
 
+    
 
 def get_timestamp():
     #first get UTC time, then convert to EST to avoid issues with daylight savings
@@ -65,6 +121,17 @@ def get_timestamp():
     
     tz = pytz.timezone(TIMEZONE)
     local_time = now.astimezone(tz)
+
+    print(local_time)
     
-    dt_string = local_time.strftime("%m/%d/%Y %H:%M:%S")
-    return dt_string
+    #dt_string = local_time.strftime("%m/%d/%Y %H:%M:%S")
+
+    
+    #return dt_string
+    return local_time
+
+
+
+
+
+
--- Web Files/submitsystem/submission_backend_tests.py
+++ Web Files/submitsystem/submission_backend_tests.py
@@ -1,17 +1,20 @@
 from submission_backend import * #get functions from submission_backend.py
-from db_func import *
 from section_management import *
+from submitsystem.db_func import *
+import time
 
 
 ###################
 ###Constants
 ###################
 
-FILENAME_TEST = "Web Files/submitsystem/test files\hello_world.py"
-COLLECTION_NAME = "sections_test"
+ASSIGNMENT_ID = "hello_world"
+FILENAME_TEST = "test files\hello_world.py"
+COLLECTION_NAME = "CMSC 201"
 STUDENT_ID = "12345"
 STUDENT_NAME = "Queen Victoria"
 SECTION_NUM = 5
+DUE_DATE = "2020-10-31 23:59" #change appropriately to test "late submission" messages
 
 
 if __name__ == "__main__":
@@ -20,12 +23,28 @@ if __name__ == "__main__":
 
     add_student(STUDENT_ID, STUDENT_NAME, SECTION_NUM, COLLECTION_NAME)
 
-    list_collection(COLLECTION_NAME) #should now have one student
+    add_assignment(SECTION_NUM, ASSIGNMENT_ID, FILENAME_TEST, DUE_DATE, COLLECTION_NAME)
+
+    list_collection(COLLECTION_NAME) #should now have one student and one assignment
     
-    ret_val = submit_file(STUDENT_ID, SECTION_NUM, FILENAME_TEST, COLLECTION_NAME)
+    ret_val = submit_file(STUDENT_ID, ASSIGNMENT_ID, SECTION_NUM, FILENAME_TEST, COLLECTION_NAME)
     print("This should be 0: ", ret_val)
 
     list_collection(COLLECTION_NAME) #should now have submission in Victoria's submissions list
+
+    #add 3 more submissions and get most recent
+    print("Adding 3 more submissions...\n")
+    for i in range(3):
+        time.sleep(5) #wait 5 seconds
+        submit_file(STUDENT_ID, ASSIGNMENT_ID, SECTION_NUM, FILENAME_TEST, COLLECTION_NAME)
+        print("submission made at ", time.ctime())
+
+    #should now have 4 submissions
+    list_collection(COLLECTION_NAME)
+
+    print()
+    latest_sub = get_latest_submission(STUDENT_ID, SECTION_NUM, COLLECTION_NAME)
+    print("Latest submission from " + STUDENT_NAME + " in " + COLLECTION_NAME + ", section" , SECTION_NUM  , " is: " , latest_sub)
 
     drop_collection(COLLECTION_NAME)
 
